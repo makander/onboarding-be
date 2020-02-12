4609d492c2ca46699f1c4237af365101c659d3e4
Nils <nils@mw-008.local>
2020-02-12 13:17:49 +0100

    setup dotenv and configure sequelize

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-// import Express from 'express'
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const Sequelize = require('sequelize');
 const routes = require('./routes');
 
-
 const app = express();
+
+const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`);
+
+sequelize.authenticate().then(() => { console.log('connected'); })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
