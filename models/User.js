3e5b6de816860a62c4bb78a36eb137cb1e6bc507
Nils <nils@mw-008.local>
2020-02-14 10:58:54 +0100

    add bcrypt to user model to hash password in db

--- models/User.js
+++ models/User.js
@@ -1,4 +1,12 @@
 
+const bcrypt = require('bcrypt');
+
+const options = {
+  salt: 10,
+};
+const saltRounds = 10;
+const secret = 'Mmmh,IPA';
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
@@ -6,7 +14,14 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     role: DataTypes.ENUM('Admin', 'Guest'),
-  }, {});
+  },
+  {});
+
+  User.beforeCreate(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+    user.password = hashedPassword;
+  });
+
   User.associate = function (models) {
     // associations can be defined here
   };
