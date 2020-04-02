54a9b265cbf3d37e3265b42ee63d8a8bf97609e7
Nils Makander <makander@gmail.com>
2020-04-02 18:50:40 +0200

    uibuild

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const router = express.Router();
 
-router.use(express.static(path.join(__dirname, 'build')));
+router.use(express.static(path.join(__dirname, '/../build')));
 
 router.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
