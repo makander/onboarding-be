179aac7657a38d021fcb1f0c7d053491ea33edbb
Nils Makander <makander@gmail.com>
2020-04-17 22:04:34 +0200

    be-deploy

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.use(express.static(path.resolve(__dirname, '/../build')));
 
-router.get('/', function (req, res) {
+router.get('/*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '/../build', 'index.html'));
 });
 
